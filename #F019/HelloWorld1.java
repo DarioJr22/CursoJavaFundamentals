cdb7c33b69679c761234f46f76e61fd9fa37566b
Dário <dariojr1233@gmail.com>
2023-03-21 22:17:14 -0300

    Hello Word - Java

---
+++ #F019/HelloWorld1.java
@@ -0,0 +1,15 @@
+public class HelloWorld1{
+
+    public static void main(String[] args){
+        /* System.out.print("Mi amigo," + args[0] + " " + args[1] +" estás bien ?"  ); */
+        String ambiente = args[0];
+            if(ambiente.equals("DEV")){
+                System.out.print("Executando em Dev");
+            } else if(ambiente.equals("TESTE")){
+                System.out.print("Executando em Teste");
+            }else{
+                System.out.print("O nome dela é maura, aquela que machucou meu coração");
+            }
+    }
+
+}
